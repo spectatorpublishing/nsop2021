303f6a8d361d6bf4ec1ebb058c76ed8f676a999b
Erin <erin2722@gmail.com>
2021-08-26 14:38:49 -0400

    initial navbar layout

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,25 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-    font-family: 'Noto Sans', sans-serif;
+    margin: 2rem;
+    display: flex;
+    justify-content: space-between;
 `;
 
-const Header = styled.h1`
+const Header = styled.div`
+    font-family: 'Noto Sans', sans-serif;
     text-transform: uppercase;
-    font-weight: bold;
+    font-weight: 600;
+    font-size: 2.5rem;
 `;
 
 const SubHeader = styled.h4`
-    font-family: 'Noto Sans', sans-serif;
+    font-family: 'Amiko', sans-serif;
+    font-size: 1rem;
+`;
+
+const Logo = styled.img`
+    width: 15rem;
 `;
 
 const NavBar = () => {
@@ -21,7 +30,9 @@ const NavBar = () => {
                 <SubHeader>Select an option below to find out more about your orientation guide.</SubHeader>
             </div>
             <div>
-                
+                <a href="https://www.columbiaspectator.com/">
+                    <Logo src="https://cds-archives.s3.amazonaws.com/pb/resources/img/spectator-logo.png"></Logo>
+                </a>
             </div>
         </Wrapper>
     )
