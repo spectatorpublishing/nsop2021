91af898657b3b9f063941591b4243e9d0f513d9d
sarahbraka <69284826+sarahbraka@users.noreply.github.com>
2021-08-26 18:51:02 -0400

    Update ArticleData.js

--- src/data/ArticleData.js
+++ src/data/ArticleData.js
@@ -1,40 +1,70 @@
 export const Articles = {
     "Academics": [
         {
-            title: "Why Sarah Braka is the best wingwoman",
-            author: "Me",
-            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
-            link: ""
+            title: "Barnard requirements: A student’s crash course",
+            author: "Tian Griffin",
+            imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/X6BAHUIE2VA5NB5HYFDIM7UCYY.JPG",
+            link: "https://www.columbiaspectator.com/spectrum/2018/09/17/barnard-requirements-a-students-crash-course/"
         },
         {
-            title: "Why Sarah Braka is the best wingwoman",
-            author: "Me",
-            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
-            link: ""
+            title: "What classes will you take during your first year in SEAS?",
+            author: "Victoria Yang",
+            imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/YUVYTJ5J7ZBULPO3XQ6M3FGIBE",
+            link: "https://www.columbiaspectator.com/required-reading/2017/04/13/what-classes-will-you-take-during-your-first-year-in-seas/"
         },
         {
-            title: "Why Sarah Braka is the best wingwoman",
-            author: "Me",
-            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
-            link: ""
+            title: "What to do if you’re on a waitlist",
+            author: "Abhishek Hariharan",
+            imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/OVBHCQGTHVHKRFNOHRS2YTFULE.png",
+            link: "https://www.columbiaspectator.com/spectrum/2019/12/04/what-to-do-if-youre-on-a-waitlist/"
         },
         {
-            title: "Why Sarah Braka is the best wingwoman",
-            author: "Me",
-            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
-            link: ""
+            title: "First-year fears: Solutions for common academic worries",
+            author: "Veronica Grace Taleon",
+            imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/OLYRFGF4M5FLTP2GBATFRW5CWE.png",
+            link: "https://www.columbiaspectator.com/required-reading/2017/08/14/first-year-fears-solutions-for-common-academic-worries/"
         },
         {
-            title: "Why Sarah Braka is the best wingwoman",
-            author: "Me",
-            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
-            link: ""
+            title: "Advice for incoming transfer students to Columbia and Barnard",
+            author: "Izzy Mollicone",
+            imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/F7AOM4ZPNZAP5IQCOWD3QKZAVM.jpg",
+            link: "https://www.columbiaspectator.com/spectrum/2019/05/19/advice-for-incoming-transfer-students-to-columbia-and-barnard/"
         },
         {
-            title: "Why Sarah Braka is the best wingwoman",
-            author: "Me",
-            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
-            link: ""
+            title: "At the core of Columbia’s academic scene: What’s up with the Core Curriculum for Columbia College?",
+            author: "Huber Gonzalez",
+            imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/KNUL2JPFIVCODHLKC56YTX4EAA",
+            link: "https://www.columbiaspectator.com/spectrum/2017/03/31/at-the-core-of-cus-academic-scene-whats-up-with-the-core-curriculum-for-columbia-college/"
+        },
+        {
+            title: "Dos and don’ts of the first week of classes",
+            author: "Ria Honda",
+            imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/YAWDMXHTGVHZHEOV745FERFK3I.jpg",
+            link: "https://www.columbiaspectator.com/spectrum/2019/05/20/dos-and-donts-of-first-week-of-classes/"
+        },
+        {
+            title: "Academic advice for incoming transfer students",
+            author: "Nandini Talwar",
+            imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/JUUQCG5C2JAF7M4Z232KL7DGYE.jpg",
+            link: "https://www.columbiaspectator.com/spectrum/2020/08/20/academic-advice-for-incoming-transfer-students/"
+        },
+        {
+            title: "Guide to tutoring services",
+            author: "Lina Bennani Karim",
+            imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/JSWGQKG3DRCU3IDDRCDRZRDQ6E.jpg",
+            link: "https://www.columbiaspectator.com/spectrum/2019/10/04/guide-to-tutoring-services/"
+        },
+        {
+            title: "Registration 101: From tips on choosing your classes to making a 4-year plan",
+            author: "Swethaa Suresh and Nirutti Charupaisankit",
+            imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/D26VXPRCGFHOFCYWPWKKZ5LX4E.jpeg",
+            link: "https://www.columbiaspectator.com/spectrum/2020/11/30/registration-101-from-tips-on-choosing-your-classes-to-making-a-4-year-plan/"
+        },
+        {
+            title: "Spectrum’s guide to building the perfect schedule",
+            author: "Grace Lin",
+            imgLink: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/DPX7VALB5RFQZDJGBVDMNUQYPE.jpg",
+            link: "https://www.columbiaspectator.com/spectrum/2018/08/18/spectrums-guide-to-building-the-the-perfect-schedule/"
         },
     ],
     "Covid-19": [
