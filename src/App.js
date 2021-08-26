4e635e2824ae3768006db645c6f8bf97014ecd12
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2021-08-26 16:04:00 -0400

    Implemented Like What You See 2 Component, Added component to App.js for testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import NavBar from './components/NavBar';
+import LikeWhatYouSee2 from './components/LikeWhatYouSee2';
 
 function App() {
   return (
     <div className="App">
         <NavBar />
+        <LikeWhatYouSee2/>
     </div>
   );
 }
