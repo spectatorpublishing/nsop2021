b15ba6beb4e83fc1b154a89f913af4895dcaf527
Erin <erin2722@gmail.com>
2021-08-26 16:14:10 -0400

    more stuff

---
+++ src/data/ArticleData.js
@@ -0,0 +1,154 @@
+export const Articles = {
+    "Academics": [
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+    ],
+    "Covid-19": [
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+    ],
+    "Student Life": [
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+    ],
+    "Explore Housing": [
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+        {
+            title: "Why Sarah Braka is the best wingwoman",
+            author: "Me",
+            imgLink: "https://images.squarespace-cdn.com/content/v1/53bf2a9be4b05a05f0bd263c/1629325063894-V5MX0HBCNKY5CD3LIY2V/sarah+braka?format=1000w",
+            link: ""
+        },
+    ],
+};
