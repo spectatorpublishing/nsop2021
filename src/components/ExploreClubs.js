4b87a86b486fa495507b3777301eeb9df2bea360
Francesca Cavuoti <fc2637@columbia.edu>
2021-08-27 11:00:23 +0200

    Added LionClubs button with working link

--- src/components/ExploreClubs.js
+++ src/components/ExploreClubs.js
@@ -8,13 +8,14 @@ const BigWrap = styled.div`
 const Wrapper = styled.div`
     background-color: #E0E2DC;
     display: flex;
-    margin-right: 2rem;
-    margin-left: 2rem;
     padding: 2rem;
     margin: 3rem;
     height: fit-content;
     border-radius: 5px;
 
+    a {
+        text-decoration: none;
+    }
 
     @media (max-width: 425px) {
         margin-right: 0rem;
@@ -37,6 +38,19 @@ const Paragraph = styled.div`
     }
 `;
 
+const ClubsButton = styled.div`
+    background: #D37462;
+    padding: 0.7rem;
+    border-radius: 5px;
+    color: #FFFFFF;
+    width: fit-content;
+    height: fit-content;
+    margin: 0;
+    text-transform: uppercase;
+    font-weight: 600;
+    justify-self: end;
+`;
+
 const ExploreClubs = () => {
     return (
         <BigWrap>
@@ -48,6 +62,9 @@ const ExploreClubs = () => {
                 Spectator’s newest platform created by our own product and 
                 engineering teams.
                 </Paragraph>
+                <a href="http://www.lionclubs.info">
+                    <ClubsButton>Visit LionClubs</ClubsButton>
+                </a>
             </Wrapper>
         </BigWrap>
     )
