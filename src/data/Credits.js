6c3b62c1465551723005a8ffd97bb14bbcd545a5
sarahbraka <69284826+sarahbraka@users.noreply.github.com>
2021-08-27 12:49:54 -0400

    Update Credits.js

--- src/data/Credits.js
+++ src/data/Credits.js
@@ -8,11 +8,275 @@ const credits = {
             "name": "Lizzie Karpen",
             "position": "Managing Editor"
         }
+        {
+            "name": "Tamarah Wallace",
+            "position": "Publisher"
+        }
     ],
     "News": [
         {
             "name": "Dia Gill",
             "position": "News Editor"
         }
-    ]
+        {
+            "name": "Talia Abrahamson",
+            "position": "Deputy News Editor"
+        }
+        {
+            "name": "Stella Pagkas",
+            "position": "Staff Writer"
+        }
+        {
+            "name": "Irie Sentner",
+            "position": "Staff Writer"
+        }
+    ],
+    "Opinion": [
+        {
+            "name": "Ryan Oden",
+            "position": "Editorial Page Editor"
+        },
+    ],
+    "Sports": [
+        {
+            "name": "Mackenzie George",
+            "position": "Sports Editor"
+        },
+        {
+            "name": "Clara Ence Morse",
+            "position": "Sports Editor"
+        }
+        {
+            "name": "Jorge Hernandez",
+            "position": "Deputy Sports Editor"
+        }
+        {
+            "name": "Matt Kim",
+            "position": "Deputy Sports Editor"
+        }
+        {
+            "name": "Miles Schachner",
+            "position": "Deputy Sports Editor"
+        }
+        {
+            "name": "Alex Kwok",
+            "position": "Staff Writer"
+        }
+        {
+            "name": "Rebecca Wachen",
+            "position": "Staff Writer"
+        }
+        {
+            "name": "Bernard Wang",
+            "position": "Staff Writer"
+        }
+    ],
+    "A&E": [
+        {
+            "name": "Noah Sheidlower",
+            "position": "Arts & Entertainment Editor"
+        },
+        {
+            "name": "Sophie Craig",
+            "position": "Deputy A&E Editor"
+        }
+        {
+            "name": "Olivia Doyle",
+            "position": "Deputy A&E Editor"
+        }
+        {
+            "name": "Bella Druckman",
+            "position": "Deputy A&E Editor"
+        }
+        {
+            "name": "Katie Levine",
+            "position": "Deputy A&E Editor"
+        }
+        {
+            "name": "Fernanda Aguero",
+            "position": "Staff Writer"
+        }
+        {
+            "name": "Noah Bullwinkle",
+            "position": "Staff Writer"
+        }
+        {
+            "name": "Jane Loughman",
+            "position": "Staff Writer"
+        }
+        {
+            "name": "Irene Madrigal",
+            "position": "Staff Writer"
+        }
+    ],
+    "Copy": [
+        {
+            "name": "Emma Bradbury",
+            "position": "Head Copy Editor"
+        },
+        {
+            "name": "Kaylene Chong",
+            "position": "Deputy Copy Editor"
+        }
+        {
+            "name": "Vincenzo DiNatale",
+            "position": "Deputy Copy Editor"
+        }
+        {
+            "name": "Robyn Fohouo",
+            "position": "Deputy Copy Editor"
+        }
+        {
+            "name": "Mabry Griffin",
+            "position": "Deputy Copy Editor"
+        }
+        {
+            "name": "Harry Shi",
+            "position": "Deputy Copy Editor"
+        }
+        {
+            "name": "Olivia Vella",
+            "position": "Deputy Copy Editor"
+        }
+        {
+            "name": "Maria Levit",
+            "position": "Associate Copy Editor"
+        }
+        {
+            "name": "Natalie Park",
+            "position": "Associate Copy Editor"
+        }
+        {
+            "name": "Adam Burns",
+            "position": "Preslotter"
+        }
+    ],
+    "Graphics": [
+        {
+            "name": "Jessica Li",
+            "position": "Graphics Editor"
+        },
+        {
+            "name": "Elizabeth Commisso",
+            "position": "Deputy Graphics Editor"
+        }
+        {
+            "name": "Charlotte Li",
+            "position": "Deputy Graphics Editor"
+        }
+    ],
+    "Photo": [
+        {
+            "name": "Beatrice Shlansky",
+            "position": "Photo Editor"
+        },
+        {
+            "name": "Samantha Camacho",
+            "position": "Deputy Photo Editor"
+        }
+        {
+            "name": "Kate Della Pietra",
+            "position": "Deputy Photo Editor"
+        }
+        {
+            "name": "Millie Felder",
+            "position": "Deputy Photo Editor"
+        }
+        {
+            "name": "Elisabeth McLaughlin",
+            "position": "Deputy Photo Editor"
+        }
+        {
+            "name": "Rommel Nunez",
+            "position": "Deputy Photo Editor"
+        }
+        {
+            "name": "Sarah Jayne Austin",
+            "position": "Staff Photographer"
+        }
+        {
+            "name": "Emma Chen",
+            "position": "Staff Photographer"
+        }
+        {
+            "name": "Temi George",
+            "position": "Staff Photographer"
+        }
+        {
+            "name": "Princeton Huang",
+            "position": "Staff Photographer"
+        }
+        {
+            "name": "Alex Kieu",
+            "position": "Staff Photographer"
+        }
+        {
+            "name": "Olivia Treynor",
+            "position": "Staff Photographer"
+        }
+        {
+            "name": "Selina Yang",
+            "position": "Staff Photographer"
+        }
+    ],
+    "Illustrations": [
+        {
+            "name": "Brenda Huang",
+            "position": "Illustrations Editor"
+        },
+        {
+            "name": "Liza Evseeva",
+            "position": "Deputy Illustrations Editor"
+        }
+        {
+            "name": "Ashley Jiao",
+            "position": "Deputy Illustrations Editor"
+        }
+        {
+            "name": "Carlos Ochoa",
+            "position": "Deputy Illustrations Editor"
+        }
+        {
+            "name": "Yingjie Wang",
+            "position": "Deputy Illustrations Editor"
+        }
+        {
+            "name": "Tasneed Bashir",
+            "position": "Staff Illustrator"
+        }
+        {
+            "name": "Noelle Hunter",
+            "position": "Staff Illustrator"
+        }
+        {
+            "name": "Tina Wang",
+            "position": "Staff Illustrator"
+        }
+        {
+            "name": "Helen Yang",
+            "position": "Staff Illustrator"
+        }
+    ],
+    "Podcasts": [
+        {
+            "name": "Samuel Hyman",
+            "position": "Podcast Director"
+        },
+        {
+            "name": "Matthew Lucia",
+            "position": "Deputy Composer"
+        }
+        {
+            "name": "Claire Schnatterbeck",
+            "position": "Deputy Script Editor"
+        }
+        {
+            "name": "Natalie Goldberg",
+            "position": "Associate Script Editor"
+        }
+        {
+            "name": "Avery Reed",
+            "position": "Associate Script Editor"
+        }
+    ],
 };
